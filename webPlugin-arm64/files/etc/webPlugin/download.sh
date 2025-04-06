3e1b9c10a087029cedbc816a19bc6e19796e778d
lieoxc <xiaocongli45@gmail.com>
2025-04-06 16:43:08 +0800

    增加clash内核以及mips arm插件分别下载不同的内核

--- webPlugin-arm64/files/etc/webPlugin/download.sh
+++ webPlugin-arm64/files/etc/webPlugin/download.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 [ -f "/etc/openclash/core/clash_tun" ] || {
-		wget http://files.sd-web.top:33626/openclashCore.tar.gz -O /tmp/openclashCore.tar.gz
+		wget http://files.sd-web.top:33626/openclashCore-arm64.tar.gz -O /tmp/openclashCore-arm64.tar.gz
 
-		[ -f "/tmp/openclashCore.tar.gz" ] && {
+		[ -f "/tmp/openclashCore-arm64.tar.gz" ] && {
             mkdir -p /tmp/new
             mkdir -p /etc/openclash/core/
-			tar -zxvf /tmp/openclashCore.tar.gz -C /tmp/new/
-            cp /tmp/new/openclashCore/* /etc/openclash/core/
+			tar -zxvf /tmp/openclashCore-arm64.tar.gz -C /tmp/new/
+            cp /tmp/new/openclashCore-arm64/* /etc/openclash/core/
 		}
 		
-		rm -f /tmp/openclashCore.tar.gz
+		rm -f /tmp/openclashCore-arm64.tar.gz
 		
 }
--- webPlugin-mips/files/etc/webPlugin/download.sh
+++ webPlugin-mips/files/etc/webPlugin/download.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 [ -f "/etc/openclash/core/clash_tun" ] || {
-		wget http://files.sd-web.top:33626/openclashCore.tar.gz -O /tmp/openclashCore.tar.gz
+		wget http://files.sd-web.top:33626/openclashCore-mips.tar.gz -O /tmp/openclashCore-mips.tar.gz
 
-		[ -f "/tmp/openclashCore.tar.gz" ] && {
+		[ -f "/tmp/openclashCore-mips.tar.gz" ] && {
             mkdir -p /tmp/new
             mkdir -p /etc/openclash/core/
-			tar -zxvf /tmp/openclashCore.tar.gz -C /tmp/new/
-            cp /tmp/new/openclashCore/* /etc/openclash/core/
+			tar -zxvf /tmp/openclashCore-mips.tar.gz -C /tmp/new/
+            cp /tmp/new/openclashCore-mips/* /etc/openclash/core/
 		}
 		
-		rm -f /tmp/openclashCore.tar.gz
+		rm -f /tmp/openclashCore-mips.tar.gz
 		
 }
